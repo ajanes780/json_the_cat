a4d6b71714d5b68a2bb255b557a17c1aa6c7e53f
Aaron Janes <Aaron.Janes@gmail.com>
2021-01-14 09:36:15 -0700

    added user input

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,13 @@
 const request = require('request');
+const userInput = process.argv.slice(2)
 
-request('https://api.thecatapi.com/v1/images/search?breed_ids=sibe', function(err, res, body) {
-    console.log(typeof body);
-    console.log( body)
+request('https://api.thecatapi.com/v1/images/search?breed_ids='+userInput[0], function(err, res, body) {
+    // console.log(typeof body);
+    // console.log( body)
 
     request('response', function( data )  {
       data = JSON.parse(body);
-      console.log(data);
+      console.log(data[0]);
       console.log(typeof data);
 
 
