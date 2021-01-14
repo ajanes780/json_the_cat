707552ef9877151ac51fed4d67ee688f064e146e
Aaron Janes <Aaron.Janes@gmail.com>
2021-01-14 09:46:20 -0700

    added some basic error handling

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,16 @@
 const request = require('request');
 const userInput = process.argv.slice(2)
 
-request('https://api.thecatapi.com/v1/images/search?breed_ids='+userInput[0], function(err, res, body) {
-    // console.log(typeof body);
-    // console.log( body)
-
-    request('response', function( data )  {
+request('https://api.thecatapi.com/v1/images/search?breed_ids='+userInput[0], function(error, res, body) {
+    
+        
+    request('response', function( data, error  )  {
       data = JSON.parse(body);
       console.log(data[0]);
       console.log(typeof data);
-
-
+      console.error('error:', error); // Print the error if one occurred
+      if(error === undefined){
+        console.log(`This animal ${userInput[0]} is not defined please try again`);
+      }
     });
-
   });
