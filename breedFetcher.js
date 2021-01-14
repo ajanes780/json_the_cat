ec1b79d3411f3513413a4d97d0415c7d52af04b4
Aaron Janes <Aaron.Janes@gmail.com>
2021-01-14 10:12:42 -0700

    linted

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,17 @@
 const request = require('request');
-const userInput = process.argv.slice(2)
+const userInput = process.argv.slice(2);
 
-request('https://api.thecatapi.com/v1/images/search?breed_ids='+userInput[0], function(error, res, body) {
+request('https://api.thecatapi.com/v1/images/search?breed_ids=' + userInput[0], function(error, res, body) {
     
         
-    request('response', function( data, error  )  {
-      data = JSON.parse(body);
+  request('response', function(data,)  {
+    data = JSON.parse(body);
+    if (data[0] === undefined) {
+      console.log(`The animal ${userInput[0]} is not defined please try again`);
+    }
+    if (error === null)  {
       console.log(data[0]);
-      console.log(typeof data);
-      console.error('error:', error); // Print the error if one occurred
-      if(error === undefined){
-        console.log(`This animal ${userInput[0]} is not defined please try again`);
-      }
-    });
+    }
+    
   });
+});
